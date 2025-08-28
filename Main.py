658b00b80bf4654ebf2632fa382c1d21882afb65
Deus William Carino <deuscarino@gmail.com>
2025-08-27 21:59:40 -0400

    Start of The UI with Pyqt

--- Main.py
+++ Main.py
@@ -1,37 +1,83 @@
 from module import dbmodules as useModule
 
+import json
 
-# names =data["Firstname"]
-# #print(len(data["Firstname"]))
-# listOFNames = []
-# listOFCountry = []
-# filename = "records try.csv"
-# rowsforcsv = ["Full Name","Country"]
-# dataToCSV = []
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow,QVBoxLayout, QLabel,QHBoxLayout,QTextEdit
+
+import sys
 
 
 
-# #print(useModule.onlyUnique(names))
-# fnamesList =db_col.firstnames
-# lnamesList = db_col.lastnames
-# countryList = db_col.countrylist
-
-# for x in range (1000):
-#     fname = fnamesList[random.randint(0,len(fnamesList)-1)]
-#     lname = lnamesList[random.randint(0,len(lnamesList)-1)]
-#     country = countryList[random.randint(0,len(countryList)-1)]
-
-#     wholename = fname + " " + lname 
-#     listOFNames.append(wholename)
-#     listOFCountry.append(country)
+with open('data/buttonDetails.json', 'r') as f:
+    buttonDetails  = json.load(f)
 
 
-# for x in range(len(listOFNames)-1):
-#         dataToCSV.append([listOFNames[x],listOFCountry[x]])
-# # writing to csv file
-# with open(filename, 'w') as csvfile:
-#     # creating a csv writer object
-#     csvwriter = csv.writer(csvfile)
-#     # writing the fields
-#     csvwriter.writerow(rowsforcsv)
-#     csvwriter.writerows(dataToCSV)
+selectedButton = ""
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Database Creator")
+        
+        mainLabel = QLabel("Dabase Creator")
+        mainLabelFont = mainLabel.font()
+        mainLabelFont.setPointSize(30)
+        mainLabel.setFont(mainLabelFont)
+        mainLabel.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+        titlebar = QVBoxLayout()
+        titlebar.addWidget(mainLabel)
+
+        #=======================================#
+
+        namebtn = QPushButton("Names")
+        namebtn.clicked.connect(lambda:self.clicked_Button("Names",infoOfButton))
+
+        countrybtn = QPushButton("Button")
+        countrybtn.clicked.connect(lambda:self.clicked_Button("Country",infoOfButton))
+
+        genderbtn = QPushButton("Gender")
+        genderbtn.clicked.connect(lambda:self.clicked_Button("Gender",infoOfButton))
+
+        
+        buttonLayout = QHBoxLayout()
+        buttonLayout.addWidget(namebtn)
+        buttonLayout.addWidget(countrybtn)
+        buttonLayout.addWidget(genderbtn)
+
+        #=======================================#
+
+        infoOfButton = QTextEdit()
+        infoOfButton.setPlaceholderText("Select A Button To try")
+        infoOfButton.isReadOnly
+
+        infoSelectedLayout = QVBoxLayout()
+        infoSelectedLayout.addWidget(infoOfButton)
+        #=======================================#
+
+        selectionArea = QHBoxLayout()
+        selectionArea.addLayout(buttonLayout)
+        selectionArea.addLayout(infoSelectedLayout)
+        #=======================================#
+
+        mainLayout = QVBoxLayout()
+        mainLayout.addLayout(titlebar)
+        mainLayout.addLayout(selectionArea)
+
+
+        container = QWidget()
+        container.setLayout(mainLayout)
+
+        self.setCentralWidget(container)
+        
+    def clicked_Button(self,name,txtLayout):
+        txtLayout.setText(buttonDetails[name])
+
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show() 
+
+app.exec()
